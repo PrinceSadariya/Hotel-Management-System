ba535d1af1392a38796aee0691cadcb17e51c97d
Sprince196 <sadariyaprince3516@gmail.com>
2023-01-09 12:13:27 +0530

    Database Credentials Removed

--- model/_constant.php
+++ model/_constant.php
@@ -1,5 +1,5 @@
 <script>
-    const SITE_URL = "http://final_project.prince.sadariya.radixusers2.com/";
+    const SITE_URL = "WRITE_HERE_YOUR_SERVER_PATH";
 </script>
 
 <?php
@@ -8,6 +8,6 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-define("SITE_URL", "http://final_project.prince.sadariya.radixusers2.com/");
+define("SITE_URL", "WRITE_HERE_YOUR_SERVER_PATH");
 
-define("SENDGRID_API_KEY", "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");
+define("SENDGRID_API_KEY", "SENDGRID_API_KEY_HERE");
